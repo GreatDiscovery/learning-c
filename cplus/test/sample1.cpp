c48efb011429f8ceb2dfd33132b138d88fc0a18b
qiangzhi <1303535630@qq.com>
2022-07-29 23:49:47 +0800

    add: gtest example

--- cplus/test/sample1.cpp
+++ cplus/test/sample1.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
-#include <string>
-#include "gtest/gtest.h"
-using namespace std;
+//
+// Created by Gavin on 2022/7/29.
+//
+
+#include "sample1.h"
 
 // Returns n! (the factorial of n).  For negative n, n! is defined to be 1.
 int Factorial(int n) {
@@ -37,12 +38,16 @@ bool IsPrime(int n) {
     return true;
 }
 
+// Tests factorial of 0.
+TEST(FactorialTest, HandlesZeroInput) {
+cout << "start test" <<endl;
+EXPECT_EQ(Factorial(0), 1);
+}
 
-
-
-
-//int main() {
-//    std::cout << "Hello, World!" << std::endl;
-//    return 0;
-//}
-
+// Tests factorial of positive numbers.
+TEST(FactorialTest, HandlesPositiveInput) {
+EXPECT_EQ(Factorial(1), 1);
+EXPECT_EQ(Factorial(2), 2);
+EXPECT_EQ(Factorial(3), 6);
+EXPECT_EQ(Factorial(8), 40320);
+}
