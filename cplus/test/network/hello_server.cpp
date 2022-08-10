7462f67fc200d638181f39d0e8cce2f591df1923
qiangzhi <1303535630@qq.com>
2022-08-11 00:32:06 +0800

    update: network program
    complete server socket.

--- cplus/test/network/hello_server.cpp
+++ cplus/test/network/hello_server.cpp
@@ -6,11 +6,14 @@
 #include <gtest/gtest.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-using namespace std;
 
 void error_handling(char *message) {
-    cout << message << endl;
+    std::cout << message << std::endl;
     exit(1);
+}
+
+void success_handling(char *message) {
+    std::cout << message << std::endl;
 }
 
 TEST(server, case1) {
@@ -22,10 +25,40 @@ TEST(server, case1) {
     server_socket_fd = socket(PF_INET, SOCK_STREAM, 0);
     if (server_socket_fd == -1) {
         error_handling("socket() error");
+    } else {
+        success_handling("socket() success");
     }
-//    bind();
-//    listen();
-//    accept();
 
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(atoi("9999"));
+    //调用 bind 函数分配ip地址和端口号
+    if (bind(server_socket_fd, (struct sockaddr *) &server_address, sizeof(server_address)) == -1) {
+        error_handling("bind() error");
+    } else {
+        success_handling("bind() success");
+    }
+    //调用 listen 函数将套接字转为可接受连接状态
+    if (listen(server_socket_fd, 10)) {
+        error_handling("listen() error");
+    } else {
+        success_handling("listen() success");
+    }
+    // accept连接
+    socklen_t client_addr_size = sizeof(client_address);
+    client_socket_fd = accept(server_socket_fd, (struct sockaddr *) &client_address, &client_addr_size);
+    if (client_socket_fd == -1) {
+        error_handling("accept() error");
+    } else {
+        success_handling("accept() success");
+    }
+
+    char message[] = "hello socket";
+    write(client_socket_fd, message, sizeof(message));
+
+    // close连接
+    close(client_socket_fd);
+    close(server_socket_fd);
 }
 
