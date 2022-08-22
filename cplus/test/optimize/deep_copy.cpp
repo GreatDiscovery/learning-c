0e9b73e9fcc2f59d43b997d824309f7b20a502b2
qiangzhi <songbowen.sbw@alibaba-inc.com>
2022-08-22 13:16:37 +0800

    update: std::move

--- cplus/test/optimize/deep_copy.cpp
+++ cplus/test/optimize/deep_copy.cpp
@@ -2,6 +2,7 @@
 // Created by Gavin on 2022/8/21.
 //
 
+#include <list>
 #include "../basic.h"
 
 namespace deep_copy {
@@ -43,5 +44,17 @@ namespace deep_copy {
 
     TEST(deep_copy_test, 重复删除指针报错) {
         A a = Get(false);
+    }
+
+//    应用move 语义调用构造函数，就避免了拷贝，提高了程序性能。当一个对象内部有较大的堆内存或者动态数组时，
+//    很有必要写move 语义的拷贝构造函数和赋值函数，避免无谓的深拷贝，以提高性能
+    TEST(move_test, move语义和作用) {
+        {
+            std::list<std::string> tokens;
+            // todo 计时比较
+            std::list<std::string> t = tokens;
+        }
+        std::list<std::string> tokens;
+        std::list<std::string> t = std::move(tokens);
     }
 }
