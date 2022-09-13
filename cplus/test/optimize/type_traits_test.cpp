316e6d8605f093c1c4a2ce7f9d3cd459acafd889
qiangzhi <songbowen.sbw@alibaba-inc.com>
2022-09-13 13:13:15 +0800

    update: type_traits result_of

--- cplus/test/optimize/type_traits_test.cpp
+++ cplus/test/optimize/type_traits_test.cpp
@@ -39,3 +39,32 @@ TEST(traits_test, 判断类型相同) {
     cout << is_same<int, unsigned int>::value << endl;
     cout << is_same<int, signed int>::value << endl;
 }
+
+int fn(int) {
+    return int();
+}
+
+typedef int (&fn_ref) (int); // 函数引用
+typedef int (*fn_ptr) (int); // 函数指针
+// 仿函数
+struct fn_class {
+    int operator() (int i) {
+        return i;
+    }
+};
+
+TEST(traits_test, 编译期获取一个可调用对象的返回类型) {
+    typedef std::result_of<decltype(fn) &(int)>::type A;
+    typedef result_of<fn_ref(int)>::type B;
+    typedef result_of<fn_ptr(int)>::type C;
+    typedef result_of<fn_class(int)>::type D;
+
+    // 可以转换boolean输出类型为true和false
+    cout << boolalpha;
+    cout << "typedefs of int:" << endl;
+
+    cout << "A: " << is_same<int, A>::value << endl;
+    cout << "B: " << is_same<int, B>::value << endl;
+    cout << "C: " << is_same<int, C>::value << endl;
+    cout << "D: " << is_same<int, D>::value << endl;
+}
