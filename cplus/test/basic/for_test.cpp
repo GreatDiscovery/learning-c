7daf2175f5c899d88e2d190d6c2fe95c8fb5fe76
songbowen <1303535630@qq.com>
2024-04-21 20:38:18 +0800

    update: gtest

--- cplus/test/basic/for_test.cpp
+++ cplus/test/basic/for_test.cpp
@@ -5,6 +5,7 @@
 #include "gtest/gtest.h"
 #include <iostream>
 #include "vector"
+#include <unistd.h>
 
 int global_var = 10;
 
@@ -47,3 +48,33 @@ TEST(for_case, for3) {
     ASSERT_NO_THROW(modify_elements());
 }
 
+typedef std::map<std::string, std::string> UserCollectedProperties;
+
+TEST(for_case, for4) {
+    char hostname[256]; // 声明一个数组来存储主机名
+    if (gethostname(hostname, sizeof(hostname)) == 0) { // 获取主机名
+        std::cout << "Hostname: " << hostname << std::endl; // 打印主机名
+    } else {
+        std::cerr << "Error: Unable to get hostname" << std::endl; // 打印错误信息
+        return; // 返回错误码
+    }
+    for (const std::string& prefix : {"num.111", "num.333", "num.555"}) {
+        cout << prefix << endl;
+    }
+
+    UserCollectedProperties props{{"num.111.1", "1"},
+                                  {"num.111.2", "2"},
+                                  {"num.111.3", "3"},
+                                  {"num.333.1", "1"},
+                                  {"num.333.2", "2"},
+                                  {"num.333.3", "3"},
+                                  {"num.555.1", "1"},
+                                  {"num.555.2", "2"},
+                                  {"num.555.3", "3"}, };
+    for (const std::string& prefix :
+            {"num.000", "num.222", "num.444", "num.666"}) {
+        auto pos = props.lower_bound(prefix);
+        ASSERT_TRUE(pos == props.end() ||
+                    pos->first.compare(0, prefix.size(), prefix) != 0);
+    }
+}
