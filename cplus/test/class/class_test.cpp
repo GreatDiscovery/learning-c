b8446c94064ee31508bae3be10d7bd61362817fd
songbowen <1303535630@qq.com>
2024-04-21 15:06:37 +0800

    add: explicit keyword

---
+++ cplus/test/class/class_test.cpp
@@ -0,0 +1,29 @@
+//
+// Created by 宋博文 on 2024/4/21.
+//
+
+#include <iostream>
+#include <gtest/gtest.h>
+
+class MyClass {
+public:
+    explicit MyClass(int x) : value(x) {}
+
+    int getValue() const { return value; }
+
+private:
+    int value;
+};
+
+void printValue(const MyClass &obj) {
+    std::cout << "Value: " << obj.getValue() << std::endl;
+}
+
+
+TEST(MyClass, explicit_test) {
+    // explicit 是一个关键字，用于声明单参数构造函数为显式构造函数，这意味着该构造函数不会自动进行隐式类型转换。
+//    MyClass obj1 = 10; // 错误，不能进行隐式类型转换
+//    printValue(obj1);
+    MyClass obj2(20); // 正确，直接调用构造函数进行初始化
+    printValue(obj2);
+}
