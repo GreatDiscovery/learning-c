2390e123521a1e7a760f20b8ad4a21dee6af079e
jiayun <songbowen@xiaohongshu.com>
2024-04-29 14:50:18 +0800

    update: move & forward

--- cplus/test/optimize/forward_test.cpp
+++ cplus/test/optimize/forward_test.cpp
@@ -8,7 +8,7 @@
 #include "../basic.h"
 
 template<typename T>
-void PrintT(T &t) {
+void PrintT(T &t) { // 当同时有Print(T &t)和PrintT(T &&t)时，由于传入形参Print(x)，就会命中左值引用的PrintT(T &t)
     std::cout << "left value" << std::endl;
 }
 
@@ -24,7 +24,7 @@ void TestForward(T &&v) {
     PrintT(std::move(v));
 }
 
-// 例子不是太好
+
 TEST(forward_test, 转发优化) {
     TestForward(1);
     std::cout << "==============================" << std::endl;
@@ -36,6 +36,7 @@ TEST(forward_test, 转发优化) {
 }
 
 using namespace std;
+
 template<typename T>
 void func(T &&param) {
     if (std::is_same<string, T>::value)
@@ -68,4 +69,68 @@ TEST(forward_test, 引用折叠) {
     func(1); // 传递参数是右值 T推导成了int, 所以是int&& param, 右值引用
     func(x); // 传递参数是左值 T推导成了int&, 所以是int& && param, 折叠成 int&,左值引用
     func(getInt());// 参数getInt是右值 T推导成了int, 所以是int&& param, 右值引用
+}
+
+template<typename T>
+void foo(T &&param) {
+    if (std::is_rvalue_reference<decltype(param)>::value)
+        std::cout << "rvalue reference\n";
+    else std::cout << "lvalue reference\n";
+}
+
+// case: https://zhuanlan.zhihu.com/p/469607144
+TEST(forward_test, 引用折叠2) {
+    int a = 0;
+    foo(a);
+    foo(std::move(a));
+}
+
+class A {
+public:
+    A() {}
+
+    A(size_t size) : size(size), array((int *) malloc(size)) {
+        std::cout
+                << "create Array，memory at: "
+                << array << std::endl;
+
+    }
+
+    ~A() {
+        free(array);
+    }
+
+    A(A &&a) : array(a.array), size(a.size) {
+        a.array = nullptr;
+        std::cout
+                << "Array moved, memory at: "
+                << array
+                << std::endl;
+    }
+
+    A(A &a) : size(a.size) {
+        array = (int *) malloc(a.size);
+        for (int i = 0; i < a.size; i++)
+            array[i] = a.array[i];
+        std::cout
+                << "Array copied, memory at: "
+                << array << std::endl;
+    }
+
+    size_t size;
+    int *array;
+};
+
+template<typename T>
+void warp(T &&param) {
+    if (std::is_rvalue_reference<decltype(param)>::value) {
+        std::cout << "param is rvalue reference\n";
+    } else std::cout << "param is lvalue reference\n";
+    A y = A(param); // 右值如果不使用forward转发，由于形参存在实际的地址空间则进一步的函数调用会被当作左值
+    A z = A(std::forward<T>(param)); // 防止右值变为左值，
+}
+
+TEST(forward_test, 完美转发Test) {
+    A a = A(100);
+    warp(std::move(a)); // 右值在函数转发过程中，容易重新变为左值。
 }
