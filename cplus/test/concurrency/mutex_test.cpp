4e1c0831a8add59a68888892c294e675a48e06bc
songbowen <1303535630@qq.com>
2024-08-17 22:07:37 +0800

    add: std::lock_guard

--- cplus/test/concurrency/mutex_test.cpp
+++ cplus/test/concurrency/mutex_test.cpp
@@ -4,6 +4,32 @@
 //
 
 #include "../basic.h"
+#include <thread>
+
+std::mutex checking_mutex_;
+int total = 0;
+
+void addNum() {
+    for (int i = 0; i < 10; i++) {
+        {
+            std::lock_guard<std::mutex> lock(checking_mutex_);
+            total += 1;
+        }
+    }
+}
+
+
+TEST(mutext_test, 测试lock) {
+    std::vector<std::thread> threads;
+    for (int i = 0; i < 5; ++i) {
+        threads.emplace_back(addNum);
+    }
+
+    for (auto &thread: threads) {
+        thread.join();
+    }
+    std::cout << total << std::endl;
+}
 
 struct foo {
     int f_count;
@@ -23,3 +49,4 @@ struct foo *foo_alloc(int id) {
     }
     return fp;
 }
+
