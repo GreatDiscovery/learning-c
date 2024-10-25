eb6aaa70fd40dadbc7125898fd32998a79f72ca8
songbowen <1303535630@qq.com>
2024-10-25 22:40:27 +0800

    add: ## keyword

--- cplus/test/basic/macro_test.cpp
+++ cplus/test/basic/macro_test.cpp
@@ -17,6 +17,10 @@ the world"                  // 多行宏
     cout << "hello world!" << endl;                    \
 } while(0)
 
+// ## 符号可以在需要动态生成变量名、结构体字段名、或者函数名等场景下应用
+#define RTE_PER_LCORE(name) (per_lcore_##name)
+int per_lcore_my_var = 10;
+
 TEST(macra_test, 宏测试) {
     cout << "M=" << M << endl;
     cout << "HELLO=" << HELLO << endl;
@@ -25,4 +29,9 @@ TEST(macra_test, 宏测试) {
     printf("COUNT = %d\n", COUNT(x + 1));// 输出结果： COUNT = 13
     printf("COUNT = %d\n", COUNT(++x));  // 输出结果： COUNT = 56
     hello();
+}
+
+TEST(macra_test, 双井号作用) {
+    int value = RTE_PER_LCORE(my_var);  // 展开后为 (per_lcore_my_var)
+    assert(value == 10);
 }
